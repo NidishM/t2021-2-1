b2294513ada40575c66ea5a6d119f260f395031c
Nidish <nidishmuraleedharan@gmail.com>
2022-09-21 23:48:14 +0530

    updated

--- Program-3.py
+++ Program-3.py
@@ -11,3 +11,17 @@
 #         .
 #         7) input a = x, then output : 1, 3, 5, 7, .......
 
+
+def oddsOutput(a):
+    if(a%2 == 0):
+        a = a - 1
+    oddsArr = []
+    i = 1
+    while len(oddsArr) != a:
+        oddsArr.append(i)
+        i += 2
+    return oddsArr
+
+a = int(input('Please enter the number: '))
+output = oddsOutput(a)
+print(*output, sep=',')
